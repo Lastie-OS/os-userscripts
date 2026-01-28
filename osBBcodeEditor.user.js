27cf1fb32f3da2231dcd06a88bb770d8eb40b573
LastieOS <lastie.onseq@gmail.com>
2026-01-28 16:03:19 -0600

    Update version number to 3

--- osBBcodeEditor.user.js
+++ osBBcodeEditor.user.js
@@ -2,7 +2,7 @@
 // @name         OS Chat BBCode Editor
 // @icon         https://github.com/Lastie-OS/os-userscripts/blob/main/icon.png?raw=true
 // @namespace    http://tampermonkey.net/
-// @version      2.8
+// @version      3
 // @description  OS BBcode editor for chat... what else do you need to know???
 // @author       Lastie
 // @match        https://onlinesequencer.net/forum/chat_frame.php*
