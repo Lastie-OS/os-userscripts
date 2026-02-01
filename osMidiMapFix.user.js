a55b134d6a5562c3bcb4d0f2978585649cb40460
LastieOS <lastie.onseq@gmail.com>
2026-02-01 16:12:36 -0600

    Update osMidiMapFix.user.js

--- osMidiMapFix.user.js
+++ osMidiMapFix.user.js
@@ -6,6 +6,7 @@
 // @description  Fixes the OS Midi Export by setting the midiInstrumentMap variable to match General MIDI standards (1-128)
 // @author       Lastie
 // @match        *://*.onlinesequencer.net/*
+// @match        *://seq.onl/*
 // @grant        GM_addStyle
 // @run-at       document-idle
 // @updateURL    https://github.com/Lastie-OS/os-userscripts/raw/refs/heads/main/osMidiMapFix.user.js
