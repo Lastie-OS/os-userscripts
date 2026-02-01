0fa01a8ef60121e3ed01ed4c6adc07db587fe803
LastieOS <lastie.onseq@gmail.com>
2026-02-01 16:11:38 -0600

    Update osChatNameStyler.user.js

--- osChatNameStyler.user.js
+++ osChatNameStyler.user.js
@@ -2,10 +2,11 @@
 // @name         OS Chat Name Styler
 // @icon         https://github.com/Lastie-OS/os-userscripts/blob/main/icon.png?raw=true
 // @namespace    https://lastie-os.github.io/os-userscripts/
-// @version      1.28.2026.19
+// @version      2.1.2026
 // @description  Customizable name styles
 // @author       Lastie
 // @match        https://onlinesequencer.net/forum/chat_frame.php*
+// @match         *.://seq.onl/forum/chat_frame.php*
 // @grant        GM_addStyle
 // @run-at       document-idle
 // @downloadURL  https://github.com/Lastie-OS/os-userscripts/raw/refs/heads/main/osChatNameStyler.user.js
